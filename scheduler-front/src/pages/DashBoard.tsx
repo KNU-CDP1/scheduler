d9370d8d3289084947e4bdb8035d6716178256c7
Kadrick <kbk2581553@gmail.com>
2024-11-09 12:26:55 +0900

    dev : add table exmaple

--- scheduler-front/src/pages/DashBoard.tsx
+++ scheduler-front/src/pages/DashBoard.tsx
@@ -1,10 +1,68 @@
-import { VStack, Button, Flex, Heading } from "@chakra-ui/react";
+import { VStack, Button, Flex, Heading, Table, Text } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../components/layout/Layout";
 import { HiUpload } from "react-icons/hi";
 
+const mockData: DashBoard[] = [
+  {
+    flight: "SKY101",
+    passengers: 2,
+    seats: 4,
+    cost: 80,
+    departure: "9:00 AM",
+    arrival: "10:30 AM",
+    weather: "Sunny",
+    risk: 20,
+    status: "Completed",
+  },
+  {
+    flight: "SKY102",
+    passengers: 5,
+    seats: 6,
+    cost: 250,
+    departure: "11:00 AM",
+    arrival: "12:30 PM",
+    weather: "Rainy",
+    risk: 40,
+    status: "InFlight",
+  },
+  {
+    flight: "SKY103",
+    passengers: 4,
+    seats: 4,
+    cost: 150,
+    departure: "1:00 PM",
+    arrival: "2:30 PM",
+    weather: "Sunny",
+    risk: 10,
+    status: "Delayed",
+  },
+  {
+    flight: "SKY104",
+    passengers: 1,
+    seats: 4,
+    cost: 40,
+    departure: "3:00 PM",
+    arrival: "4:00 PM",
+    weather: "Windy",
+    risk: 30,
+    status: "Delayed",
+  },
+  {
+    flight: "SKY105",
+    passengers: 3,
+    seats: 3,
+    cost: 150,
+    departure: "5:00 PM",
+    arrival: "6:00 PM",
+    weather: "Cloudy",
+    risk: 50,
+    status: "Cancelled",
+  },
+];
+
 const DashBoard: React.FC<{}> = () => {
-  const hdandleImage = (file: File) => {
+  const handleImage = (file: File) => {
     console.log(file.name);
   };
 
@@ -17,6 +75,29 @@ const DashBoard: React.FC<{}> = () => {
             <HiUpload /> Upload CSV
           </Button>
         </Flex>
+        {/*================================ Table Example ==============================*/}
+        <Table.Root>
+          <Table.Header>
+            <Table.Row background={"blackAlpha.300"} border={"1px white"}>
+              <Table.ColumnHeader>
+                <Text color={"white"}>Flight</Text>
+              </Table.ColumnHeader>
+              <Table.ColumnHeader>
+                <Text color={"white"}>Passengers</Text>
+              </Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {mockData.map((data, idx) => {
+              return (
+                <Table.Row background={"blackAlpha.300"}>
+                  <Table.Cell>{data.flight}</Table.Cell>
+                  <Table.Cell>{data.cost}</Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table.Root>
       </VStack>
     </Layout>
   );
--- scheduler-front/src/props.d.ts
+++ scheduler-front/src/props.d.ts
@@ -1,2 +1,21 @@
 /*================================================================================================*/
+// api props
+
+/*================================================================================================*/
 // component props
+
+/*================================================================================================*/
+// data
+interface DashBoard {
+  flight: string;
+  passengers: number;
+  seats: number;
+  cost: number;
+  //   departure: Date;
+  //   arrival: Date;
+  departure: string;
+  arrival: string;
+  weather: string; // ? fix
+  risk: number;
+  status: "Completed" | "InFlight" | "Delayed" | "Cancelled";
+}
